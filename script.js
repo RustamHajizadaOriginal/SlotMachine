dca212f58615e577e8c7578f970307b4d87d4fa0
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-04-09 21:00:12 +0200

    Updating JS file

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // 1. Deposit some money
+// 2.Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won or lost
+// 6. Give the user their winnings
+// 7. Ask the user if they want to play again
